9ded659c90adf0648f28998c575789609168db25
Shrinath Prabhu <shrinath.prabhu@hashbinary.com>
2020-10-27 22:21:06 +0530

    Added router view

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <template>
-  <landing-page />
+  <transition name="fade">
+    <router-view />
+  </transition>
 </template>
 
-<script>
-import LandingPage from "./views/landing-page.vue";
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.25s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
 
+<script>
 export default {
   name: "App",
-
-  components: {
-    LandingPage,
-  },
 
   mounted: function () {
     document.title = "Credenshare";
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,27 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import LandingPage from '../views/landing-page.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Landing Page',
+    component: LandingPage
   },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => 
+  // },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '*',
+    name: 'Redirect Invalid Paths to landing page',
+    redirect: '/'
   }
 ]
 
@@ -24,6 +29,6 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
