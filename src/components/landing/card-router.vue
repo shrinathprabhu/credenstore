734f48df6bddd498dd8ef30c338bbd69e3a2622b
Shrinath Prabhu <shrinath.prabhu@hashbinary.com>
2020-10-27 20:49:43 +0530

    Added landing page and card router

---
+++ src/components/landing/card-router.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-tooltip bottom>
+    <template v-slot:activator="{ on, attrs }">
+      <v-card
+        elevation="0"
+        outlined
+        shaped
+        height="150px"
+        class="pa-3 text-center"
+        width="180px"
+        style="cursor: pointer"
+        :style="
+          $vuetify.theme.dark ? 'border-color: white' : 'border-color: #757575'
+        "
+        v-on="on"
+        v-bind="attrs"
+        :class="crClass ? crClass : ''"
+      >
+        <div class="mt-2">
+          <v-icon v-if="icon" size="80px">{{ icon }}</v-icon>
+        </div>
+        <div class="mt-2">
+          <span class="subtitle-2">{{ name }}</span>
+        </div>
+      </v-card>
+    </template>
+    <span>{{ description }}</span>
+  </v-tooltip>
+</template>
+
+<script>
+export default {
+  name: "card-router",
+  props: ["name", "description", "icon", "crClass"],
+};
+</script>
---
+++ src/views/landing-page.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container>
+    <landing-description />
+    <v-row class="justify-center">
+      <card-router
+        icon="mdi-lock"
+        name="Store Credentials"
+        description="Encrypt your credentials and store it on cloud"
+      ></card-router>
+      <card-router
+        crClass="ml-10"
+        icon="mdi-key-variant"
+        name="Retrieve Credentials"
+        description="Download your credentials from cloud and decrypt it"
+      ></card-router>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import LandingDescription from "../components/landing/landing-description.vue";
+import CardRouter from "../components/landing/card-router.vue";
+export default {
+  name: "landing-page",
+  components: {
+    LandingDescription,
+    CardRouter,
+  },
+  data: () => ({}),
+};
+</script>
